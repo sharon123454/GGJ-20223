b436d894ab9f26d8e310548be688a28f55e655a2
DorShemTov <83244591+DorShemTov@users.noreply.github.com>
2023-02-03 00:45:56 +0200

    Bounciness 2d

---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    [SerializeField] private int index = 0;
+    [SerializeField] private Transform piecePos;
+    [SerializeField] bool isMoving = false;
+
+    void Update()
+    {
+        if (isMoving)
+        {
+            MovePiece();
+        }
+
+    }
+    void MovePiece()
+    {
+        transform.position = Input.mousePosition;
+    }
+}
