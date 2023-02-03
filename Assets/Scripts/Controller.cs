c71d5ecc8f238a81e7831639a70e14a90d867b34
Eli Zaidman-PC <Zaidmaneli@gmail.com>
2023-02-03 16:04:52 +0200

    Camera and stuff

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float sensitivity = 2.0f;
+    public float dashSpeed = 20.0f;
+    public float dashDuration = 0.5f;
+    public float skinWidth = 0.1f;
+    public LayerMask obstacleMask;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 mousePosition;
+    private float dashTime;
+    private bool isDashing;
+
+    private CapsuleCollider capsuleCollider;
+    private Vector3 movementDirection;
+
+    void Start()
+    {
+        capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        movementDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        movementDirection *= speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+        {
+            dashTime = Time.time + dashDuration;
+            isDashing = true;
+        }
+
+        if (isDashing)
+        {
+            movementDirection = movementDirection.normalized * dashSpeed * Time.deltaTime;
+            if (Time.time >= dashTime)
+            {
+                isDashing = false;
+            }
+        }
+
+        float distance = movementDirection.magnitude + skinWidth;
+        RaycastHit[] hits = Physics.SphereCastAll(transform.position, capsuleCollider.radius, movementDirection, distance, obstacleMask);
+        if (hits.Length > 0)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                if (!hit.collider.isTrigger)
+                {
+                    movementDirection = Vector3.zero;
+                    break;
+                }
+            }
+        }
+
+        transform.Translate(movementDirection, Space.World);
+
+        Vector3 lookDirection = movementDirection;
+        lookDirection.y = 0;
+        if (lookDirection.magnitude > 0)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * sensitivity);
+        }
+    }
+}
