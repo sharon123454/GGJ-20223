1e7d52a4544cb56c5b4ec5c457dd9ae1db0800f2
Eli Zaidman-PC <Zaidmaneli@gmail.com>
2023-02-04 15:11:12 +0200

    no message

--- Assets/TimeUntilNextGame.cs
+++ Assets/TimeUntilNextGame.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TimeUntilNextGame : MonoBehaviour
 {
@@ -19,13 +20,18 @@ public class TimeUntilNextGame : MonoBehaviour
     void Update()
     {
         realTime += Time.deltaTime;
+        if (SceneManager.GetActiveScene().buildIndex < 3)
+        {
+            timeText.text = $"{realTime} / {_time}";
+        }
+
         timeText.text = $"{realTime.ToString("0")}/ {_time.ToString("0")}";
 
         if (_time <= realTime && !wentIn)
         {
             wentIn = true;
-            
-             AsyncOperationProgressExample.Instance.StartNewGame();
+
+            AsyncOperationProgressExample.Instance.StartNewGame();
         }
 
     }
