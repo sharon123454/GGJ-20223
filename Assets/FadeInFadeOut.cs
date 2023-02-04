a3c1eda1935d2bf5481a13efcb2ef0c7466ed8f3
Eli Zaidman-PC <Zaidmaneli@gmail.com>
2023-02-04 11:53:18 +0200

    no message

---
+++ Assets/FadeInFadeOut.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInFadeOut : MonoBehaviour
+{
+    float t = 0;
+    float _t = 0;
+    float duration = 1f;
+    float _duration = 1f;
+    public Image _image;
+    public AnimationCurve anim;
+    public GameObject FirstClicked;
+    public GameObject SecondClicked;
+    public IEnumerator CameraLerEnumerator()
+    {
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            _image.color = Color.Lerp(_image.color, new Color(_image.color.r, _image.color.g, _image.color.b, 1), anim.Evaluate(t / (duration * 15)));
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+        SecondClicked.SetActive(true);
+        while (_t < _duration)
+        {
+            _t += Time.deltaTime;
+            _image.color = Color.Lerp(_image.color, new Color(_image.color.r, _image.color.g, _image.color.b, 0), anim.Evaluate(t / (duration * 15)));
+            yield return null;
+        }
+        FirstClicked.SetActive(false);
+
+    }
+    public void LerpIn()
+    {
+        StartCoroutine(CameraLerEnumerator());
+    }
+}
