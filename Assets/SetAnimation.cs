41476e2588f84c51f0014d6252926f20bf5d4bc5
Eli Zaidman-PC <Zaidmaneli@gmail.com>
2023-02-04 07:42:05 +0200

    no message

--- Assets/SetAnimation.cs
+++ Assets/SetAnimation.cs
@@ -28,6 +28,7 @@ public class SetAnimation : MonoBehaviour
                 skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Back Idle", true);
 
             }
+<<<<<<< HEAD
         }
         else
         {
@@ -38,6 +39,144 @@ public class SetAnimation : MonoBehaviour
                 fartPack.transform.rotation =
                     Quaternion.Euler(90, fartPack.transform.rotation.y, fartPack.transform.rotation.z);
                 skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Front Idle", true);
+=======
+
+            if (Input.GetKey(KeyCode.D) && !DashenteredButtomRight)
+            {
+                DashenteredButtomLeft = false;
+                DashenteredButtomRight = true;
+
+                DashenteredTopLeft = false;
+                DashenteredTopRight = false;
+
+                enteredTopLeft = false;
+                enteredTopRight = false;
+                enteredButtomLeft = false;
+                enteredButtomRight = false;
+
+                fartPack.transform.rotation =
+                    Quaternion.Euler(-90, fartPack.transform.rotation.y, 45);
+                skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Back Right Dash", false);
+                print("BACK RIGHT DASH AAAAAAAAA");
+            }
+            //else if (Input.GetKey(KeyCode.W) && !DashenteredButtomRight)
+            //{
+            //    DashenteredButtomLeft = false;
+            //    DashenteredButtomRight = false;
+
+            //    DashenteredTopLeft = false;
+            //    DashenteredTopRight = false;
+
+            //    enteredTopLeft = false;
+            //    enteredTopRight = false;
+            //    enteredButtomLeft = false;
+            //    enteredButtomRight = false;
+
+            //    fartPack.transform.rotation =
+            //        Quaternion.Euler(-90, fartPack.transform.rotation.y, 0);
+            //    skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Back Right Dash", false);
+            //    print("BACK RIGHT DASH AAAAAAAAA");
+            //}
+        }
+        else
+        {
+
+            if (!controller.isDashing)
+            {
+                if (Input.GetKey(KeyCode.A) && !enteredTopLeft)
+                {
+                    enteredTopLeft = true;
+                    enteredTopRight = false;
+
+                    enteredButtomLeft = false;
+                    enteredButtomRight = false;
+
+                    DashenteredButtomLeft = false;
+                    DashenteredButtomRight = false;
+                    DashenteredTopLeft = false;
+                    DashenteredTopRight = false;
+
+                    skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Front Walk Left", true);
+                    print("FRONT LEFT");
+
+                }
+
+                if (Input.GetKey(KeyCode.D) && !enteredTopRight)
+                {
+                    enteredTopLeft = false;
+                    enteredTopRight = true;
+
+                    enteredButtomLeft = false;
+                    enteredButtomRight = false;
+
+                    DashenteredButtomLeft = false;
+                    DashenteredButtomRight = false;
+                    DashenteredTopLeft = false;
+                    DashenteredTopRight = false;
+                    skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Front Walk Right", true);
+                    print("FRONT RIGHT");
+                }
+            }
+
+            else
+            {
+                if (Input.GetKey(KeyCode.A) && !DashenteredTopLeft)
+                {
+                    fartPack.transform.rotation =
+                        Quaternion.Euler(90, fartPack.transform.rotation.y, -45);
+                    DashenteredTopLeft = true;
+                    DashenteredTopRight = false;
+
+                    DashenteredButtomLeft = false;
+                    DashenteredButtomRight = false;
+
+                    enteredTopLeft = false;
+                    enteredTopRight = false;
+                    enteredButtomLeft = false;
+                    enteredButtomRight = false;
+
+                    skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Front Left Dash", false);
+                    print("FRONT LEFT DASH");
+
+                }
+
+                if (Input.GetKey(KeyCode.D) && !DashenteredTopRight)
+                {
+                    fartPack.transform.rotation =
+                        Quaternion.Euler(90, fartPack.transform.rotation.y, 45);
+                    DashenteredTopRight = true;
+                    DashenteredTopLeft = false;
+
+                    DashenteredButtomLeft = false;
+                    DashenteredButtomRight = false;
+
+                    enteredTopLeft = false;
+                    enteredTopRight = false;
+                    enteredButtomLeft = false;
+                    enteredButtomRight = false;
+
+                    skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Front Right Dash", false);
+                    print("FRONT RIGHT DASH");
+                }
+                //if (Input.GetKey(KeyCode.S) && !DashenteredTopRight)
+                //{
+                //    DashenteredButtomLeft = false;
+                //    DashenteredButtomRight = false;
+
+                //    DashenteredTopLeft = false;
+                //    DashenteredTopRight = false;
+
+                //    enteredTopLeft = false;
+                //    enteredTopRight = false;
+                //    enteredButtomLeft = false;
+                //    enteredButtomRight = false;
+
+                //    fartPack.transform.rotation =
+                //        Quaternion.Euler(90, fartPack.transform.rotation.y, 0);
+                //    skeletonAnimation.AnimationState.SetAnimation(trackIndex, "Front Right Dash", false);
+                //    print("BACK RIGHT DASH AAAAAAAAA");
+                //}
+>>>>>>> 983e8d1 (no message)
             }
         }
     }
